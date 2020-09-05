855486f27b966874f726abc88b1e30410ef450cc
tyelf22 <tyelf22@hotmail.com>
2020-09-05 13:27:14 -0600

    added post 3 FOA

--- posts.js
+++ posts.js
@@ -34,12 +34,14 @@ export default [
         id: 3,
         title: 'Functions, Objects, and Arrays',
         subtitle: 'The one with wonderful javascript',
-        body: 'This is my third nuxt post',
+        bodyOne: '<b>Intro to Functions</b><br>A good way to think of a Javascript function is to think of a small computer program. We need functions in order to get our code to execute in Javascript. I often think of functions as a recipe, you start with certain set of ingredients, then follow certain steps using the ingredients, then you will get a finished product. With that being said, functions will usually take some kind of input and return an output, however it doesn\'t always have to be structured this way. <br><br>',
         tag: 'Javascript',
         imageUrl: 'https://images.pexels.com/photos/160107/pexels-photo-160107.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        date: 'Wednesday August 27, 2020',
-        imageUrlInPost: '',
-        bodyTwo: '',
+        date: 'Saturday September 5, 2020',
+        imageUrlInPost: 'https://images.unsplash.com/photo-1518291344630-4857135fb581?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+        bodyTwo: '<br><b>Callback Functions</b><br>A callback function is simply a function that is passed as an argument to another function and can be called at a later time. This can be useful in order to write cleaner, more maintainable code. Another reason callback functions are useful is in situtations when we need code to execute after a certain event and not necessarily sequentially, this is called Asynchronous Javascript.<br><br><b>Promises</b><br>Promises are usually used for a simpler way of handling asynchronous or blocking code, for things like, API calls, DB calls, file operations, etc. They work like promises do in real life, either the promise will be fulfilled or not. If it is fulfilled then you receive what ever was promised to you, if it isn\'t fulfilled there is a reason why it wasn\'t. Well, I guess that depends on the kind of people you are making promises with, luckily in Javascript we are always guaranteed one or the other. \
+        <br><br><b>Objects</b><br>A simple definition of objects, is a collection of the same kind or different kinds of data. Objects contain key value pairs where the keys act similarly to the variable name and the values are the values of that key. The values can contain any javascript data type. Objects are defined using curly braces {}. You can easily loop through object using methods like (Object.keys, Object.values, Object.entries) \
+        <br><br><b>Arrays</b><br>Arrays are objects that can hold different data types. An array also acts as a list, a queue or a stack in JavaScript. Arrays can have items of the same data type or a mixture of different data types. Arrays are defined using brackets []. Some common ways to loop through arrays are by using a for loop, forEach, map, etc.',
     },
     {
         id: 4,
